a086cbeec98ee3f3df416ba83656fe79f8aeef1c
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-10-03 12:16:26 -0500

    eventos soportados

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {mouseX: 0, mouseY: 0 }
+  }
+
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    this.setState({mouseX: clientX, mouseY: clientY})
+  }
+
   render() {
     return (
       <div className="App">
@@ -23,6 +33,9 @@ class App extends Component {
         <div>
           <h4>Eventos</h4>
           <button onClick={() => alert('hi there !!!!!!')}>hi there !!!!</button>
+          <div onMouseMove={this.handleMouseMove} style={{ border : '1px solid #000', marginTop: 10, padding: 10 }}>
+            <p>{ this.state.mouseX } , { this.state.mouseY }</p>
+          </div>
         </div>
       </div>
     );
