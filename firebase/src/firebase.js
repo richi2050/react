83a2cd1ae4f21dae32cb59d8ffcc52ae4bd99e52
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-10-18 12:44:24 -0500

    Se aumenta  Reac

---
+++ firebase/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyAnToCVQgKcKIu41BR0PbPE3-9nz1BqyPw",
+    authDomain: "prueba-96dd5.firebaseapp.com",
+    databaseURL: "https://prueba-96dd5.firebaseio.com",
+    projectId: "prueba-96dd5",
+    storageBucket: "prueba-96dd5.appspot.com",
+    messagingSenderId: "424568790886"
+  }
+  firebase.initializeApp(config);
+  export default firebase;
